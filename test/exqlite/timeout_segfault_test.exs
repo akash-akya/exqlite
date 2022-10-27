f01563fa1131b9de923c4a2fdbafc1edfd53eaf4
akash-akya <akashh246@gmail.com>
2022-10-27 16:12:39 +0530

    Reproduce race-condition between close & prepare

--- test/exqlite/timeout_segfault_test.exs
+++ test/exqlite/timeout_segfault_test.exs
@@ -1,6 +1,8 @@
 defmodule Exqlite.TimeoutSegfaultTest do
   use ExUnit.Case
 
+  alias Exqlite.Sqlite3
+
   setup do
     {:ok, path} = Temp.path()
     on_exit(fn -> File.rm(path) end)
@@ -35,5 +37,18 @@ defmodule Exqlite.TimeoutSegfaultTest do
       end
     end)
     |> Stream.run()
+  end
+
+  test "race condition", %{path: path} do
+    {:ok, conn} = Sqlite3.open(path)
+    :ok = Sqlite3.execute(conn, "create table debug(id integer)")
+
+    spawn_link(fn ->
+      Sqlite3.prepare(conn, "insert into debug(id) values (1)")
+    end)
+
+    # ensure `Sqlite3.prepare()` is executed before we attempt to close the connection
+    Process.sleep(100)
+    :ok = Sqlite3.close(conn)
   end
 end
